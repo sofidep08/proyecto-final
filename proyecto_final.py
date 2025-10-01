fe5192bbf29edc33ef8c936b012a994142fabf08
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-09-30 22:55:26 -0600

    cargar usuario

--- proyecto_final.py
+++ proyecto_final.py
@@ -1,8 +1,41 @@
-class RegistroUsuario:
-    def __init__(self, nombre, direccion, dpi, nit, contador):
-        self.nombre = nombre
+class Usuario:
+    def __init__(self):
+        self.cargar_usuario()
+    def cargar_usuario(self):
+        try:
+            with open('usuario.txt', 'r', encoding="utf-8") as archivo:
+                for linea in archivo:
+                    linea = linea.strip()
+                    if linea:
+                        direccion, nombre, numCasa, dpi, nit, contador = linea.split(":")
+                        AgregarUsuario.usuarios[direccion] = {
+                            "Nombre": nombre,
+                            "NumCasa": numCasa,
+                            "DPI": dpi,
+                            "NIT": nit,
+                            "Contador": contador
+                        }
+            print("usuarios importados desde usuario.txt")
+        except FileNotFoundError:
+            print("El archivo usuario aun no existe, se creara automaticamente después de guardar")
+
+
+class AgregarUsuario:
+    usuarios ={}
+    def __init__(self, direccion, nombre, numCasa, dpi, nit, contador):
         self.direccion = direccion
+        self.nombre = nombre
+        self.numCasa = numCasa
         self.dpi = dpi
         self.nit = nit
         self.contador = contador
+        usuarios[self.direccion] = {
+            "Nombre": nombre,
+            "NumCasa": numCasa,
+            "DPI": dpi,
+            "NIT": nit,
+            "Contador": contador
+        }
+        self.guardar_usuario()
+        print("Se guardo el usuario con exito")
 
