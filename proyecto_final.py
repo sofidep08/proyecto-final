0b26e32a51d3aa94088dacc7a8b21ec69cbf272e
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-09-30 22:39:02 -0600

    comienzo

---
+++ proyecto_final.py
@@ -0,0 +1,8 @@
+class RegistroUsuario:
+    def __init__(self, nombre, direccion, dpi, nit, contador):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.dpi = dpi
+        self.nit = nit
+        self.contador = contador
+
