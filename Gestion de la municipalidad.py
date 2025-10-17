ba370b76f90c36f5f6f240bed7a6fa59ae57bac7
Proyectos de Pycharm <git@github.com:Aguilar-Grecia/PycharmProjects.git>
2025-10-16 22:06:07 -0600

    menu lector contador

--- Gestion de la municipalidad.py
+++ Gestion de la municipalidad.py
@@ -9,16 +9,16 @@ class Empleados:
                 for linea in archivo:
                     linea = linea.strip()
                     if linea:
-                        usuario, nombre, contraseña = linea.split(":")
+                        usuario, nombre, contrasenia = linea.split(":")
                         self.empleados[usuario] = {
                             "Nombre": nombre,
-                            "Contraseña" : contraseña
+                            "Contraseña" : contrasenia
                         }
             if not self.empleados:
                 self.crear_empleados_defecto()
             print("Empleados importados desde empleados.txt")
         except FileNotFoundError:
-            print("No existe el archivo empleados.txt, se crearán dos empleados por defecto."
+            print("No existe el archivo empleados.txt, se crearán dos empleados por defecto.")
             self.crear_empleados_defecto()
             self.guardar_empleados()
 
@@ -33,7 +33,28 @@ class Empleados:
                 archivo.write(f"{usuario}:{datos['Nombre']}\n")
         print("Empleados guardados correctamente")
 
-def menu_principal()
+    def agregar_empleado(self):
+        if len(self.empleados)  >= 2:
+            print(f"Ya existe 2 empleados contratados. No se puede agregar a nadie más. ")
+            return
+        usuario = input("Ingrese el nombre del usuario:")
+        nombre = input("Ingrese el nombre completo:")
+        contrasenia = input("Ingrese la contraseña:")
+        self.empleados[usuario] = {"Nombre": nombre, "Contraseña": contrasenia}
+        self.guardar_empleados()
+        print(f"Empleado '{usuario}' agregar correctamente.")
+
+    def mostrar_empleado(self):
+        print("\n---EMPLEADOS CONTRATADOS---")
+        for usuario, datos in self.empleados.items():
+            print(f"Usuario: {usuario}| Nombre{datos['Nombre']}")
+
+    def validar_login(self, usuario, contrasenia):
+        if usuario in self.empleados and self.empleados[usuario]["Contraseña"]== contrasenia:
+            return True
+        return False
+
+def menu_principal():
     while True:
         print("\n---PANTALLA DE INICIO---")
         print("1. Iniciar sesión")
@@ -77,4 +98,16 @@ def iniciar_sesion(rol, usuario_correcto, contraseña_correcta):
         menu_rol(rol)
     else:
         print("\n Usario o contraseña incorrectos.")
+
+def menu_rol(rol):
+    print(f"\n---MENÚ DE {rol.upper()}---")
+    if rol == "Administrar":
+        print("1. Gestionar pagos")
+    elif rol == "Lector de contador de agua":
+        print("1. Ingresar lectura del contador")
+    elif rol == "Cocodes":
+        print("1. Registrar multa")
+    input("\nPresione ENTER para regresar al inicio...")
+
+
 
